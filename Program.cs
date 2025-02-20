106363a6c0fce38ef556392acf64b5dda333c478
antoni0 <126917765+antoni0ponurko@users.noreply.github.com>
2025-02-20 12:27:11 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System;
 
-class Program
-{
-    static void Main()
-    {
-        Console.Write("Enter the price of the construction land: ");
-        double landPrice;
-        while (!double.TryParse(Console.ReadLine(), out landPrice) || landPrice < 0)
-        {
-            Console.Write("Please enter a valid non-negative number: ");
-        }
-
-        Console.Write("Enter the price of the building: ");
-        double buildingPrice;
-        while (!double.TryParse(Console.ReadLine(), out buildingPrice) || buildingPrice < 0)
-        {
-            Console.Write("Please enter a valid non-negative number: ");
-        }
-
-        double totalCost = (landPrice + buildingPrice) * 1.21;
-
-        Console.WriteLine($"The total cost of the project, including a 21% charge, is: {totalCost:F2} EUR");
-    }
-}
+Console.WriteLine("What is your name");
+string myFirstName = Console.ReadLine();
+Console.WriteLine($"Hello your name is : {myFirstName}");
+int myAge = 18;
+myAge = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Hello your name is {myFirstName} and your age is {myAge}");
